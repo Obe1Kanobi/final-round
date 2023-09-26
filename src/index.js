a3d7b5d0ac901cd49812af5b9e8d2135b1d8bcd1
Alexandr Kudinov <AmiGO_Sahar@mail.ru>
2023-09-26 17:45:17 +0300

    хз зачем, но фиксируем

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Читать далее
+
 function readMore() {
   let dot = document.querySelector(".dot");
   let more = document.querySelector(".more");
@@ -29,19 +31,98 @@ function readMore() {
   }
 }
 
-function showMore() {
-  let button = document.querySelector(".button");
-  let container = document.querySelector(".container");
-  let buttontext = document.querySelector(".buttontext");
-  let buttonimg = document.querySelector(".buttonimg");
+// Показать всё на брендах
+
+function brandsShow() {
+  let button = document.querySelector(".brands__button");
+  let container = document.querySelector(".brands__section-container");
+  let buttontext = document.querySelector(".brands__buttontext");
+  let buttonimg = document.querySelector(".brands__buttonimg");
 
   if (buttontext.innerHTML === "Скрыть") {
     container.style.display = "none";
     buttontext.innerHTML = "Показать всё";
-    buttonimg.src = "img/expand.webp";
+    buttonimg.src = "img/icon.webp";
   } else {
     container.style.display = "grid";
     buttontext.innerHTML = "Скрыть";
-    buttonimg.src = "img/expand_up.webp";
+    buttonimg.src = "img/icon_up.webp";
   }
 }
+
+// Показать всё на вью
+
+function viewShow() {
+  let button = document.querySelector(".view__button");
+  let container = document.querySelector(".view__section-container");
+  let buttontext = document.querySelector(".view__buttontext");
+  let buttonimg = document.querySelector(".view__buttonimg");
+
+  if (buttontext.innerHTML === "Скрыть") {
+    container.style.display = "none";
+    buttontext.innerHTML = "Показать всё";
+    buttonimg.src = "img/icon_up.webp";
+  } else {
+    container.style.display = "grid";
+    buttontext.innerHTML = "Скрыть";
+    buttonimg.src = "img/icon.webp";
+  }
+}
+
+// Бургер меню
+
+function burgerMenu() {
+  let aside = document.querySelector(".dopmenu");
+  aside.style.left = "0px";
+  let background = document.querySelector(".background");
+  background.style.opacity = "0.1";
+}
+
+function closeMenu() {
+  let aside = document.querySelector(".dopmenu");
+  aside.style.left = "-475px";
+  let background = document.querySelector(".background");
+  background.style.opacity = "1";
+}
+
+// Обратная связь
+
+function feedback() {
+  let aside = document.querySelector(".feedback");
+  let menu = document.querySelector(".dopmenu");
+  aside.style.display = "block";
+  menu.style.opacity = "0.1";
+  let background = document.querySelector(".background");
+  background.style.opacity = "0.1";
+}
+
+function closefeedback() {
+  let aside = document.querySelector(".feedback");
+  let menu = document.querySelector(".dopmenu");
+  aside.style.display = "none";
+  menu.style.opacity = "1";
+  let background = document.querySelector(".background");
+  background.style.opacity = "1";
+}
+
+// Звонок
+
+function call() {
+  let aside = document.querySelector(".call");
+  let menu = document.querySelector(".dopmenu");
+  aside.style.display = "block";
+  menu.style.opacity = "0.1";
+  let background = document.querySelector(".background");
+  background.style.opacity = "0.1";
+}
+
+function closecall() {
+  let aside = document.querySelector(".call");
+  let menu = document.querySelector(".dopmenu");
+  aside.style.display = "none";
+  menu.style.opacity = "1";
+  let background = document.querySelector(".background");
+  background.style.opacity = "1";
+}
+
+console.log(null + 1);
